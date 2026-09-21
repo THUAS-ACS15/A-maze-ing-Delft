7084eca54bd0bc972b9df8137bd5ce779f31811a
niilun <49104843+niilun@users.noreply.github.com>
2026-09-21 16:45:13 +0200

    Bug fix: Fixed import

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ from rooms import (
     enterTeachersRoom2,
     enterProjectRoom1,
     enterProjectRoom2,
-    enterprojectRoom2,
+    enterProjectRoom2,
     enterFrontDesk,
     enterClassroomD2015,
     enterClassroomD2035
